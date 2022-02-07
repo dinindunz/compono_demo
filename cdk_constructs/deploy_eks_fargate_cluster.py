ecc5596533712f5b3b2038028d5236521b0e0635
GitHub <dinindunz@gmail.com>
2022-02-07 23:23:48 +1300

    add node group

--- cdk_constructs/deploy_eks_fargate_cluster.py
+++ cdk_constructs/deploy_eks_fargate_cluster.py
@@ -18,6 +18,13 @@ class DeployEksCluster(Construct):
             version=_eks.KubernetesVersion.V1_21
         )
 
+        eks_cluster.add_nodegroup_capacity(
+            'node_group',
+            min_size=1,
+            max_size=2,
+            instance_types=[_ec2.InstanceType('t2.micro')]
+        )
+
         #Deploy the App
         #eks_cluster.add_manifest("mypod", {
         #    "apiVersion": "v1",
